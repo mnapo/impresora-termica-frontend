2d8e70ca7869a207ac8d119851ad9d0eb78f1ab3
mnapo <napolimatiase@gmail.com>
2025-07-27 01:58:00 -0300

    add signup page and SignupForm component

---
+++ app/signup.tsx
@@ -0,0 +1,41 @@
+import { Stack, useLocalSearchParams } from 'expo-router';
+import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native-unistyles';
+import { Container } from '~/components/Container';
+import { SignupForm } from '~/components/SignupForm';
+
+export default function Details() {
+  const { name } = useLocalSearchParams();
+
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Registro' }} />
+      <Container>
+        <View style={styles.container}>
+          <Text style={styles.title}>Crear cuenta</Text>
+          <View style={styles.separator} />
+          <SignupForm/>
+        </View>
+      </Container>
+    </>
+  );
+}
+
+const styles = StyleSheet.create((theme) => ({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 1,
+    backgroundColor: theme.colors.background,
+  },
+  separator: {
+    height: 1,
+    width: '80%',
+    backgroundColor: theme.colors.limedSpruce,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: theme.colors.typography,
+  },
+}));
