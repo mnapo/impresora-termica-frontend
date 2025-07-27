3d10fcbaaff924e2928deae0cb72c47ebf7651be
mnapo <napolimatiase@gmail.com>
2025-07-27 01:42:05 -0300

    create LoginForm component

---
+++ components/LoginForm.tsx
@@ -0,0 +1,33 @@
+import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native-unistyles';
+import { Button, Checkbox, TextInput } from 'react-native-paper';
+
+export const LoginForm = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Iniciar sesión</Text>
+      <TextInput label="Email"/>
+      <TextInput label="Clave"/>
+      <Button mode="contained">Ingresar</Button>
+      <Text><Checkbox status="unchecked"/>Recordarme</Text>
+      <Text style={styles.title}>- o ingresar mediante -</Text>
+      <Button mode="contained">Google</Button>
+      <Text style={styles.title}>- o si no tenés cuenta -</Text>
+      <Button mode="contained">Registrate</Button>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create((theme) => ({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 1,
+    backgroundColor: theme.colors.background,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: theme.colors.typography,
+  },
+}));
