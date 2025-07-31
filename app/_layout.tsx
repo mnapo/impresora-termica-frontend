f774464a47fdaf304ad5f22a1861a3c7f8b2893b
mnapo <napolimatiase@gmail.com>
2025-07-31 18:50:06 -0300

    replace index with _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,32 @@
-import { Redirect, Slot } from 'expo-router';
-import { useAuth } from './context/AuthContext';
+import React from 'react';
+import { NavigationContainer,  NavigationIndependentTree} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AuthProvider, useAuth } from './context/AuthContext';
+import LoginScreen from './(auth)/login';
+import SettingsScreen from './(tabs)/SettingsScreen';
 
-export default function RootLayout() {
+const Stack = createNativeStackNavigator();
+
+function AppNavigator() {
   const { user, loading } = useAuth();
 
   if (loading) return null;
 
-  if (!user) {
-    return <Slot />;
-  }
+  return (
+    <Stack.Navigator>
+      {user ? (
+        <Stack.Screen name="🚚 Ticketeador" component={SettingsScreen} />
+      ) : (
+        <Stack.Screen name="Ingreso" component={LoginScreen} />
+      )}
+    </Stack.Navigator>
+  );
+}
 
-  return <Slot />;
+export default function App() {
+  return (
+    <AuthProvider>
+      <AppNavigator/>
+    </AuthProvider>
+  );
 }
