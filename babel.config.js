9fb0be12ffa0f2a33d98098685b2927c485c5f6f
mnapo <napolimatiase@gmail.com>
2025-07-31 14:41:11 -0300

    remove unistyles

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
-
-  plugins.push([
-    'react-native-unistyles/plugin',
-    {
-      autoProcessRoot: 'app',
-      autoProcessImports: ['~/components'],
-    },
-  ]);
 
   return {
     presets: ['babel-preset-expo'],
-
-    plugins,
   };
 };
