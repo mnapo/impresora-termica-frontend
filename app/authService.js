c93b396ce29c7e60ef64d28434b0959fabe8a5a1
mnapo <napolimatiase@gmail.com>
2025-07-31 02:17:01 -0300

    add authentication service

---
+++ app/authService.js
@@ -0,0 +1,17 @@
+import client from './feathersClient';
+
+export async function login(email, password) {
+  return await client.authenticate({
+    strategy: 'local',
+    email,
+    password,
+  });
+}
+
+export async function logout() {
+  return await client.logout();
+}
+
+export async function reAuthenticate() {
+  return await client.reAuthenticate();
+}
