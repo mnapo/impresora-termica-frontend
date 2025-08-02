c74d09d870ea810553abc81cac0a12b17aecf86a
mnapo <napolimatiase@gmail.com>
2025-08-02 19:07:34 -0300

    add welcome message and last visited client information

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { useAuth } from '../context/AuthContext';
+import { View } from 'react-native';
+import { Text, Divider } from 'react-native-paper';
 
 export default function TabsHome() {
+  const { user } = useAuth();
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Pantalla principal de Tabs</Text>
+    <View style={styles().container}>
+      <Text style={styles().title2}>{user.email} 👋</Text>
+      <Divider style={styles().divider} />
+      <Text style={styles().title1}>Último cliente visitado</Text>
+      <Text>Sin datos</Text>
     </View>
   );
 }
+const styles = () => StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20
+  },
+  title1: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  title2: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  divider: {
+    marginVertical: 10,
+  },
+});
