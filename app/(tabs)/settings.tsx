38b9036cabe7e260546a7a7a400634fd236b343e
mnapo <napolimatiase@gmail.com>
2025-08-02 17:19:02 -0300

    add ARCA connection's state information and edit stylings

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { View, Text, TextInput } from 'react-native';
-import { Button, Checkbox, useTheme } from 'react-native-paper';
+import { Button, Chip, Icon } from 'react-native-paper';
 import { StyleSheet } from 'react-native';
 import { useAuth } from '../context/AuthContext';
 
 export default function SettingsScreen() {
-  const theme = useTheme();
   const {signOut} = useAuth();
   return (
     <View style={styles().container}>
-      <Text style={styles().title}>Settings</Text>
+      <Chip style={styles().connectionState} icon={() => (
+        <Icon source="circle" size={16} color="red" />
+      )}>conexión ARCA no establecida</Chip>
       <Button onPress={signOut} mode="contained">Cerrar sesión</Button>
     </View>
   );
@@ -18,11 +19,13 @@ export default function SettingsScreen() {
 const styles = () => StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    padding: 1,
-  },
+    padding: 20},
   title: {
     fontSize: 16,
     fontWeight: 'bold',
   },
+  connectionState: {
+    marginRight: 75,
+    marginBottom: 10
+  }
 });
