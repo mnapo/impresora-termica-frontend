577ebcf6d95926bebdad29b2c781066e475544e0
mnapo <napolimatiase@gmail.com>
2025-08-08 03:48:06 -0300

    add clients+invoices+invoicesItems reducers and sagas to rootReducer and rootSaga respectively

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { productsReducer, productsSaga } from './products';
+import { clientsReducer, clientsSaga } from './clients';
+import { invoicesReducer, invoicesSaga } from './invoices';
+import { invoicesItemsReducer, invoicesItemsSaga } from './invoices-items';
 import { all } from 'redux-saga/effects';
 
 const createSagaMiddleware = require('redux-saga').default;
@@ -7,10 +10,16 @@ const sagaMiddleware = createSagaMiddleware();
 
 const rootReducer = {
   products: productsReducer,
+  clients: clientsReducer,
+  invoices: invoicesReducer,
+  invoicesItems: invoicesItemsReducer,
 };
 
 function* rootSaga() {
   yield all([productsSaga()]);
+  yield all([clientsSaga()]);
+  yield all([invoicesSaga()]);
+  yield all([invoicesItemsSaga()]);
 }
 
 export const store = configureStore({
