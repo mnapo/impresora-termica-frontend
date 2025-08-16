a00391f21ed4e1be80eec88332743950c2aa854e
mnapo <napolimatiase@gmail.com>
2025-08-16 01:24:39 -0300

    modify client's Card.Title and Client.Subtitle, add New button and client addition Modal

--- app/components/screens/ClientsScreen.tsx
+++ app/components/screens/ClientsScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { View, FlatList } from 'react-native';
-import { TextInput, Button, Card, IconButton, Text } from 'react-native-paper';
+import { View, FlatList, StyleSheet } from 'react-native';
+import { TextInput, Button, Card, IconButton, Text, FAB, PaperProvider, Portal, Modal } from 'react-native-paper';
 import { useDispatch, useSelector } from 'react-redux';
 import { clientsActions } from '../../store/clients';
 
@@ -12,6 +12,10 @@ export default function ClientsScreen() {
   const [companyName, setCompanyName] = useState('');
   const [condIvaType, setCondIvaType] = useState('');
   const [address, setAddress] = useState('');
+
+  const [visible, setVisible] = useState(false);
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
 
   useEffect(() => {
     dispatch(clientsActions.fetchAction());
@@ -31,51 +35,71 @@ export default function ClientsScreen() {
   };
 
   return (
-    <View style={{ flex: 1, padding: 16 }}>
-      <TextInput
-        label="CUIT"
-        value={cuit}
-        onChangeText={setCuit}
-        keyboardType="numeric"
-        style={{ marginBottom: 8 }}
-      />
-      <TextInput
-        label="Razón Social"
-        value={companyName}
-        onChangeText={setCompanyName}
-        style={{ marginBottom: 8 }}
-      />
-      <TextInput
-        label="Condición IVA"
-        value={condIvaType}
-        onChangeText={setCondIvaType}
-        style={{ marginBottom: 8 }}
-      />
-      <TextInput
-        label="Dirección"
-        value={address}
-        onChangeText={setAddress}
-        style={{ marginBottom: 8 }}
-      />
-      <Button mode="contained" onPress={handleAdd}>
-        Agregar Cliente
-      </Button>
-
-      <FlatList
-        data={items}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Card style={{ marginTop: 12 }}>
-            <Card.Title title={`CUIT: ${item.cuit}`} subtitle={`RAZÓN SOCIAL: ${item.companyName}`} />
-            <Card.Actions>
-              <IconButton
-                icon="delete"
-                onPress={() => handleDelete(item.id)}
+    <PaperProvider>
+        <Portal>
+          <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={styles.modal}>
+            <View style={{ height: '100%', flex: 1, padding: 16 }}>
+              <TextInput
+                label="CUIT"
+                value={cuit}
+                onChangeText={setCuit}
+                keyboardType="numeric"
+                style={{ marginBottom: 8 }}
               />
-            </Card.Actions>
-          </Card>
-        )}
-      />
-    </View>
+              <TextInput
+                label="Razón Social"
+                value={companyName}
+                onChangeText={setCompanyName}
+                style={{ marginBottom: 8 }}
+              />
+              <TextInput
+                label="Condición IVA"
+                value={condIvaType}
+                onChangeText={setCondIvaType}
+                style={{ marginBottom: 8 }}
+              />
+              <TextInput
+                label="Dirección"
+                value={address}
+                onChangeText={setAddress}
+                style={{ marginBottom: 8 }}
+              />
+              <Button mode="contained" onPress={handleAdd}>
+                Agregar Cliente
+              </Button>
+            </View>
+          </Modal>
+        </Portal>
+      <View style={{ height: '100%' }}>
+        <FlatList
+          data={items}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Card style={{ marginTop: 12 }}>
+              <Card.Title
+                title={`CUIT: ${item.cuit} | RAZÓN SOCIAL: ${item.companyName}`}
+                subtitle={`DIRECCIÓN: ${item.companyName} | CONDICIÓN IVA: ${item.condIvaType}`}
+              />
+              <Card.Actions>
+                <IconButton
+                  icon="delete"
+                  onPress={() => handleDelete(item.id)}
+                />
+              </Card.Actions>
+            </Card>
+          )}
+        />
+        <FAB
+          icon="plus"
+          label="NUEVO"
+          onPress={showModal}
+          style={{position: 'absolute', bottom: '5%', left: '62%'}}
+        />
+      </View>
+    </PaperProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  modal: {backgroundColor: 'white', padding: 20},
+});
