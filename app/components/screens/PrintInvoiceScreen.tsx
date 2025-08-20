35aaaf415d0d25c46d8a1244930f80792bf55922
mnapo <napolimatiase@gmail.com>
2025-08-20 16:12:42 -0300

    edit printed invoice style

--- app/components/screens/PrintInvoiceScreen.tsx
+++ app/components/screens/PrintInvoiceScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { useLocalSearchParams, useRouter, Stack } from "expo-router";
 import { DataTable, Text, Divider } from "react-native-paper";
 import client from "../../feathersClient";
@@ -43,7 +43,7 @@ export default function PrintInvoiceScreen() {
 
   const onLoadedInvoice =() => {
     window.print();
-    setTimeout(() => router.push({pathname: '/(tabs)/invoices'}), 800);
+    //setTimeout(() => router.push({pathname: '/(tabs)/invoices'}), 800);
   }
 
   useEffect(() => {
@@ -57,40 +57,48 @@ export default function PrintInvoiceScreen() {
   const total = items.reduce((acc, it) => acc + (it.price || 0) * (it.quantity || 1), 0);
 
   return (
-    <View style={{ padding: 20, width: '100%' }}>
+    <View style={ styles.container }>
       <Stack.Screen options={{ headerShown: false }}/>
       {invoice.type === 'arca'?
         (<Text>Factura "A"</Text>)
         :(<Text>Comprobante</Text>)}
-      <Text style={{ fontSize: 14, marginBottom: 10 }}>
+      <Text style={ styles.invoiceTitle }>
         Nro.: #{String(invoice.id).padStart(8, '0')}
       </Text>
-      <Text style={{ fontSize: 14}}>Cliente: {clientData?.name}</Text>
-      <Text style={{ fontSize: 14}}>CUIT: {clientData?.cuit}</Text>
-      <Text style={{ fontSize: 14}}>Fecha: {new Date(invoice.createdAt).toLocaleDateString()}</Text>
-      <DataTable style={{ width: '50%', borderBottomWidth: 0 }}>
+      <Text style={ styles.invoiceInfo }>Cliente: {clientData?.name}</Text>
+      <Text style={ styles.invoiceInfo }>CUIT: {clientData?.cuit}</Text>
+      <Text style={ styles.invoiceInfo }>Fecha: {new Date(invoice.createdAt).toLocaleDateString()}</Text>
+      <DataTable style={{ width: '95%', borderBottomWidth: 0 }}>
         <DataTable.Header style={{ borderBottomWidth: 0 }}>
-          <DataTable.Title textStyle={{ fontSize: 20 }}>Producto</DataTable.Title>
-          <DataTable.Title textStyle={{ fontSize: 20 }} numeric>Cantidad</DataTable.Title>
-          <DataTable.Title textStyle={{ fontSize: 20 }} numeric>Precio</DataTable.Title>
-          <DataTable.Title textStyle={{ fontSize: 20 }} numeric>Subtotal</DataTable.Title>
+          <DataTable.Title textStyle={ styles.itemRow }>Producto</DataTable.Title>
+          <DataTable.Title textStyle={ styles.itemRow } numeric>Cantidad</DataTable.Title>
+          <DataTable.Title textStyle={ styles.itemRow } numeric>Precio</DataTable.Title>
+          <DataTable.Title textStyle={ styles.itemRow } numeric>Subtotal</DataTable.Title>
         </DataTable.Header>
 
         {items.map((item, i) => (
           <DataTable.Row key={i} style={{ borderBottomWidth: 0 }}>
-            <DataTable.Cell textStyle={{ fontSize: 20 }}>{item.name}</DataTable.Cell>
-            <DataTable.Cell textStyle={{ fontSize: 20 }} numeric>{item.quantity}</DataTable.Cell>
-            <DataTable.Cell textStyle={{ fontSize: 20 }} numeric>${item.price}</DataTable.Cell>
-            <DataTable.Cell textStyle={{ fontSize: 20 }} numeric>
+            <DataTable.Cell textStyle={ styles.itemRow }>{item.name}</DataTable.Cell>
+            <DataTable.Cell textStyle={ styles.itemRow } numeric>{item.quantity}</DataTable.Cell>
+            <DataTable.Cell textStyle={ styles.itemRow } numeric>${item.price}</DataTable.Cell>
+            <DataTable.Cell textStyle={ styles.itemRow } numeric>
               ${item.price * item.quantity}
             </DataTable.Cell>
           </DataTable.Row>
         ))}
 
         <DataTable.Row style={{ borderBottomWidth: 0 }}>
-          <DataTable.Cell textStyle={{ fontSize: 24 }}>Total: ${total}</DataTable.Cell>
+          <DataTable.Cell textStyle={{ fontSize: 26, fontWeight: 800, fontFamily: 'Ticketing' }}>Total: ${total}</DataTable.Cell>
         </DataTable.Row>
       </DataTable>
+      <Divider/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { padding: 1, width: '100%' },
+  invoiceTitle: { fontFamily: 'Ticketing', fontSize: 19, marginBottom: 10 },
+  invoiceInfo: { fontFamily: 'Ticketing', fontSize: 19 },
+  itemRow: { fontFamily: 'Ticketing', fontSize: 23, fontWeight: 500 }
+});
