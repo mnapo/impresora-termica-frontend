c1c795447dcf5a4b662d95e45fd979cffe1b6e6a
mnapo <napolimatiase@gmail.com>
2025-08-22 13:02:27 -0300

    remove clients and products tabs and edit Bottom bar style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,47 +1,46 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Icon } from 'react-native-paper';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs>
+      <Tabs initialRouteName='index' backBehavior='initialRoute'>
       <Tabs.Screen
         name="index"
         options={{
-          title: "Inicio",
-          tabBarIcon: () => <FontAwesome size={45} name="home" color="purple" />,
-          tabBarActiveTintColor: "purple",
-        }}
-      />
-      <Tabs.Screen
-        name="clients"
-        options={{
-          title: "Clientes",
-          tabBarIcon: () => <FontAwesome size={45} name="id-card-o" color="purple" />,
-          tabBarActiveTintColor: "purple",
+          title: "",
+              tabBarIcon: ({ focused, color, size }) => (
+                <Icon
+                  source={focused ? 'home' : 'home-outline'}
+                  size={40}
+                  color={'purple'}
+                />
+              )
         }}
       />
       <Tabs.Screen
         name="invoices"
         options={{
-          title: "Documentos",
-          tabBarIcon: () => <FontAwesome size={60} name="paperclip" color="purple" />,
-          tabBarActiveTintColor: "purple",
-        }}
-      />
-      <Tabs.Screen
-        name="products"
-        options={{
-          title: "Productos",
-          tabBarIcon: () => <FontAwesome size={45} name="shopping-cart" color="purple" />,
-          tabBarActiveTintColor: "purple",
+          title: "",
+              tabBarIcon: ({ focused, color, size }) => (
+                <Icon
+                  source={focused ? 'plus-circle' : 'plus-circle-outline'}
+                  size={40}
+                  color={'purple'}
+                />
+              )
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
-          title: "Cuenta",
-          tabBarIcon: () => <FontAwesome size={45} name="cog" color="purple" />,
-          tabBarActiveTintColor: "purple",
+          title: "",
+              tabBarIcon: ({ focused, color, size }) => (
+                <Icon
+                  source={focused ? 'cog' : 'cog-outline'}
+                  size={40}
+                  color={'purple'}
+                />
+              )
         }}
       />
     </Tabs>
