26d2782ca65a01ad31c61ac4b09c4884f49e7b16
mnapo <napolimatiase@gmail.com>
2025-08-24 14:02:41 -0300

    add clients and products Buttons in settings screen

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRouter } from 'expo-router'
 import { View } from 'react-native';
 import { Button, Chip, Icon } from 'react-native-paper';
 import { StyleSheet } from 'react-native';
@@ -6,12 +7,26 @@ import { useAuth } from '../context/AuthContext';
 
 export default function SettingsScreen() {
   const {signOut} = useAuth();
+  const router = useRouter();
+
+  const goToClientsScreen = () => {
+      router.push({
+          pathname: '/components/screens/ClientsScreen',
+      });
+  };
+  const goToProductsScreen = () => {
+      router.push({
+          pathname: '/components/screens/ProductsScreen',
+      });
+  };
   return (
     <View style={styles().container}>
       <Chip style={styles().connectionState} icon={() => (
         <Icon source="circle" size={16} color="green" />
       )}>conexión ARCA establecida</Chip>
-      <Button onPress={signOut} mode="contained">Cerrar sesión</Button>
+      <Button onPress={goToClientsScreen} mode="contained-tonal" style={styles().button}>Clientes</Button>
+      <Button onPress={goToProductsScreen} mode="contained-tonal" style={styles().button}>Productos</Button>
+      <Button onPress={signOut} mode="contained" style={styles().button} buttonColor="red">Cerrar sesión</Button>
     </View>
   );
 }
@@ -27,5 +42,8 @@ const styles = () => StyleSheet.create({
   connectionState: {
     marginRight: 75,
     marginBottom: 10
+  },
+  button: {
+    marginBottom: 5
   }
 });
