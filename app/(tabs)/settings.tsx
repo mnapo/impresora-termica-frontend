f2b45a9e7882e5d2b80d69e2ea1f6ad3432fd7f8
mnapo <napolimatiase@gmail.com>
2025-08-24 23:35:13 -0300

    redesign settings screen

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,31 +1,22 @@
 import React from 'react';
 import { useRouter } from 'expo-router'
 import { View } from 'react-native';
-import { Button, Chip, Icon } from 'react-native-paper';
+import { Button, Chip, Icon, Divider } from 'react-native-paper';
 import { StyleSheet } from 'react-native';
 import { useAuth } from '../context/AuthContext';
+import Option from '../components/Option'
 
 export default function SettingsScreen() {
   const {signOut} = useAuth();
-  const router = useRouter();
 
-  const goToClientsScreen = () => {
-      router.push({
-          pathname: '/components/screens/ClientsScreen',
-      });
-  };
-  const goToProductsScreen = () => {
-      router.push({
-          pathname: '/components/screens/ProductsScreen',
-      });
-  };
   return (
     <View style={styles().container}>
       <Chip style={styles().connectionState} icon={() => (
         <Icon source="circle" size={16} color="green" />
       )}>conexión ARCA establecida</Chip>
-      <Button onPress={goToClientsScreen} mode="contained-tonal" style={styles().button}>Clientes</Button>
-      <Button onPress={goToProductsScreen} mode="contained-tonal" style={styles().button}>Productos</Button>
+      <Option title="Clientes" icon="account" path="/components/screens/ClientsScreen" />
+      <Option title="Productos" icon="cart" path="/components/screens/ProductsScreen" />
+      <Divider style={{ marginVertical: 10 }}/>
       <Button onPress={signOut} mode="contained" style={styles().button} buttonColor="red">Cerrar sesión</Button>
     </View>
   );
