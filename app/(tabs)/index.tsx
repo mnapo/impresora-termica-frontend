e4371fd9ddd24e212c370d358ffb8460d09844e1
mnapo <napolimatiase@gmail.com>
2025-08-24 23:23:45 -0300

    redesign home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 import { useAuth } from '../context/AuthContext';
 import { View } from 'react-native';
 import { Text, Button, Divider, Icon } from 'react-native-paper';
+import Option from '../components/Option'
 
 export default function TabsHome() {
   const { user } = useAuth();
@@ -10,14 +11,9 @@ export default function TabsHome() {
     <View style={styles.container}>
       <Text style={styles.title2}><Icon source="account-outline" size={32}/>{user.lastName.toUpperCase()} {user.firstName.toUpperCase()}👋</Text>
       <Divider style={styles.divider} />
-      <Button style={styles.button} mode="contained-tonal">Mis facturas</Button>
-      <Button style={styles.button} mode="contained-tonal">Ver Clientes</Button>
-      <Button style={styles.button} mode="contained-tonal">Ver Productos</Button>
-      <Button style={styles.button} mode="contained-tonal">Estadísticas</Button>
-      <Button style={styles.button} mode="contained-tonal">Editar mis datos</Button>
-      <Button style={styles.button} mode="contained-tonal">Configurar estilos de impresión</Button>
-      <Divider style={styles.divider} />
-      <Text style={styles.title1}>En construcción...</Text>
+      <Option title="Mis Facturas" icon="book-multiple" path="(tabs)/invoices" />
+      <Option title="Datos de Cuenta" icon="pencil" path="(tabs)/settings" />
+      <Option title="Métricas" icon="chart-areaspline" path="" />
     </View>
   );
 }
