81855bc22925aeadf87f34801d62a1859755a467
mnapo <napolimatiase@gmail.com>
2025-08-29 22:15:39 -0300

    add Modal for invoice type selection

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
 import { useAuth } from '../context/AuthContext';
 import { View } from 'react-native';
-import { Text, Button, Divider, Icon } from 'react-native-paper';
+import { PaperProvider, Portal, Modal, Text, Button, Divider, Icon } from 'react-native-paper';
 import Option from '../components/Option'
+import InvoiceOption from '../components/InvoiceOption'
 
 export default function TabsHome() {
   const { user } = useAuth();
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title2}><Icon source="account-outline" size={32}/>{user.lastName.toUpperCase()} {user.firstName.toUpperCase()}👋</Text>
-      <Divider style={styles.divider} />
-      <View style={{ alignItems: 'center', paddingHorizontal: '10%' }}>
-        <Option title="Mis Facturas" icon="book-multiple" path="(tabs)/invoices" />
-        <Option title="Datos de Cuenta" icon="pencil" path="(tabs)/settings" />
-        <Option title="Métricas" icon="chart-areaspline" path="" />
-      </View>
+  const [visible, setVisible] = useState(false);
+  const router = useRouter();
+
+  const handlePress = ( type: string ) => {
+    setVisible(false);
+    router.push({
+        pathname: '/components/screens/NewInvoiceScreen',
+        params: { type: type },
+    });
+  };
+
+  return (<PaperProvider><Portal>
+    <Modal visible={visible} onDismiss={ () => { setVisible(false) } } contentContainerStyle={styles.modal}>
+      <Button mode="contained" onPress={() => handlePress('arca') } icon="file" style={{ marginVertical: 5}} uppercase={true} labelStyle={{ fontSize: 24 }} >Nueva factura ARCA</Button>
+      <Button mode="contained" onPress={() => handlePress('comprobante') } icon="content-paste" style={{ marginVertical: 5}} uppercase={true} labelStyle={{ fontSize: 24 }} >Nuevo comprobante</Button>
+      <Button mode="outlined" onPress={() => setVisible(false) } icon="window-close" style={{ marginVertical: 5}} uppercase={true} labelStyle={{ fontSize: 24 }} >Cerrar</Button>
+    </Modal>
+  </Portal>
+  <View style={styles.container}>
+    <Text style={styles.title2}><Icon source="account-outline" size={32}/>{user.lastName.toUpperCase()} {user.firstName.toUpperCase()}👋</Text>
+    <Divider style={styles.divider} />
+    <View style={{ alignItems: 'center', paddingHorizontal: '10%' }}>
+      <InvoiceOption title="Nuevo Comprobante" onOpenModal={() => setVisible(true)} />
+      <Option title="Datos de Cuenta" icon="pencil" path="(tabs)/settings" />
+      <Option title="Métricas" icon="chart-areaspline" path="" />
     </View>
-  );
+  </View>
+  </PaperProvider>);
 }
 const styles = StyleSheet.create({
   container: {
@@ -37,5 +56,8 @@ const styles = StyleSheet.create({
   },
   button: {
     marginVertical: 5
-  }
+  },
+  modal: {
+    backgroundColor: 'white', padding: 20
+  },
 });
