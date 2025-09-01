e6c0fb86f44e0524af6e23ccca1aec7522c4e0fe
mnapo <napolimatiase@gmail.com>
2025-08-31 22:15:52 -0300

    add user's data TextInputs to UserScreen

--- app/components/Option.tsx
+++ app/components/Option.tsx
@@ -39,6 +39,7 @@ const styles = StyleSheet.create({
     margin: 8,
     paddingVertical: 20,
     elevation: 4,
+    backgroundColor: 'white',
   },
   content: {
     alignItems: "center",
--- app/components/screens/UserScreen.tsx
+++ app/components/screens/UserScreen.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
+import { TextInput, Divider } from "react-native-paper";
 import { Stack } from "expo-router";
+import { useAuth } from '../../context/AuthContext';
 
 export default function UserScreen() {
+  const { user } = useAuth();
+  const [userEmail, setUserEmail] = useState('');
+  const [userCompanyName, setUserCompanyName] = useState('');
+  const [userAddress, setUserAddress] = useState('');
+  const [userCbu, setUserCbu] = useState('');
+  const [userAlias, setUserAlias] = useState('');
+
+  useEffect(() => {
+    setUserEmail(user.email || '');
+    setUserCompanyName(user.companyName || '');
+    setUserAddress(user.address || '');
+    setUserCbu(user.cbu || '');
+    setUserAlias(user.alias || '');
+  }, [user]);
 
   return (<>
     <Stack.Screen options={{ title: "Mis Datos" }}/>
     <View style={ styles.container } >
+      <TextInput value={userEmail} onChangeText={text => setUserEmail(text)} label="Correo" disabled={true} style={{ marginBottom: 1 }} />
+      <TextInput value={userCompanyName} onChangeText={text => setUserCompanyName(text)} disabled={true} label="Razón Social" style={{ marginBottom: 1 }} />
+      <TextInput value={userAddress} onChangeText={text => setUserAddress(text)} label="Dirección" keyboardType="numeric" style={{ marginBottom: 1 }} />
+      <TextInput value={userCbu} onChangeText={text => setUserCbu(text)} label="CBU" keyboardType="numeric" style={{ marginBottom: 1 }} />
+      <TextInput value={userAlias} onChangeText={text => setUserAlias(text)} label="Alias" style={{ marginBottom: 1 }} />
+      <Divider style={{ marginVertical: 10 }} />
     </View>
   </>);
 }
