7f6c81487b8d950a1e72085100ac4af88eaabba8
mnapo <napolimatiase@gmail.com>
2025-09-04 07:24:40 -0300

    create DeleteConfirmation component

---
+++ app/components/DeleteConfirmation.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { View, StyleSheet } from 'react-native';
+import { PaperProvider, Portal, Modal, Button, Text } from "react-native-paper";
+
+type DeleteConfirmationProps = {
+    source: string;
+    target: string;
+    visible: boolean;
+    onConfirm: () => void;
+    onDismiss: () => void;
+};
+
+export default function DeleteConfirmation({ source, target, visible, onConfirm, onDismiss }: DeleteConfirmationProps) {
+    const confirmLabel = 'Sí';
+    const cancelLabel = 'Cancelar';
+    const duration = 4000;
+    const craftMsg = () => {
+        let msgSource = '';
+        switch (source) {
+            case 'products':
+                msgSource = 'el producto';
+                break;
+            case 'clients':
+                msgSource = 'el cliente';
+                break;
+        }
+        return `¿Desea eliminar ${msgSource} ${target}?`;   
+    };
+
+    return (<Portal>
+        <Modal visible={visible} onDismiss={onDismiss} contentContainerStyle={styles.modal}>
+            <View>
+                <Text style={{ fontSize: 16 }} >{craftMsg()}</Text>
+                <Button mode="contained" onPress={onConfirm} style={styles.button}>{confirmLabel}</Button>
+                <Button mode="outlined" onPress={onDismiss} style={{ marginTop: 10 }} labelStyle={{ color: '#429E9D'}} >{cancelLabel}</Button>
+            </View>
+        </Modal>
+    </Portal>);
+}
+
+const styles = StyleSheet.create({
+  modal: { backgroundColor: 'white', padding: 20 },
+  button: { marginTop: 16, backgroundColor: 'red' }
+});
